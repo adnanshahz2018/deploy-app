bd5e116c4768be27a250afb6bd808ebc5a5ae277
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-08-04 13:32:28 +0500

    Updates

---
+++ precache-manifest.e5a88c15f619feea1f083d5e46ee52df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0b1e626187a083d30fe226eee55204",
+    "url": "/deploy-app/index.html"
+  },
+  {
+    "revision": "1dc29a33c7c2ccff5b65",
+    "url": "/deploy-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "05a52befcb72fcbf9981",
+    "url": "/deploy-app/static/js/2.cee8863b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-app/static/js/2.cee8863b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc29a33c7c2ccff5b65",
+    "url": "/deploy-app/static/js/main.3b15a34b.chunk.js"
+  },
+  {
+    "revision": "42ba769182bd3cfe489c",
+    "url": "/deploy-app/static/js/runtime-main.506b30e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
